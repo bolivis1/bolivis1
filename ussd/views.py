e5d583818e095d0cff525979dd1b35bc8d310f10
bolivis1 <bukusenge@gmail.com>
2021-12-13 15:36:55 +0200

    change

--- ussd/views.py
+++ ussd/views.py
@@ -28,94 +28,98 @@ def ussdApp(request):
         response =""
         #  main menu for our application
         if text == '':
-            response =  "CON Murakaza neza kuri Home Needs USSD app \n"
+            response =  "  CON Murakaza neza kuri Home Needs USSD app \n"
             response += "1. GUSABA DELIVERY \n"
-            response += "2. HITAMO ISOKO \n"
+            response += "2. KWISHYURA \n"
         elif text == '1':
-            # SELECT * FROM PRODUCTMODEL where title="" ORDER BY ID DESC LIMIT 5
-            fetchProducts = ProductsModel.objects.all()
-            response = "CON Hitamo igihingwa \n"
-            for  products in fetchProducts:
-                response += ""+str(products.id)+"."+str(products.title)+ "\n"
+            response= "CON HITAMO ISOKO  :\n"
+            response += "1. NYARUGENGE \n"
+            response += " 2. NYAMIRAMBO\n"
 
-        elif text == '1*1':
-            product="Ibinyomoro"
-            response = "CON shyiramo ubuso bw'ubutaka bwawe bw' "+str(product)+"\n"
-        elif category =='1*1' and int(len(level)) == 3 and str(level[2]) in  str(level):
-            response = "CON Uwo mubufatanyije \n"
-        elif category =='1*1' and int(len(level)) == 4 and str(level[3]) in  str(level):
-            response = "CON Shyiramo nimero y'irangamuntu yuwo mufatanyije \n"
-        elif category =='1*1' and int(len(level)) == 5 and str(level[4]) in  str(level):
-            # save the data into the database
-            category='Ibinyomoro'
-            sizeOfland=level[2]
-            names= level[3]
-            idnumber = level[4]
-            insert = Idafarmuser(sessiondId=session_id,
-            serviceCode = service_code,
-            phoneNumber=phone_number,
-            level=level,
-            category=category,
-            sizeOfland=sizeOfland,
-            names=names,
-            idnumber=idnumber,
-            )
-            insert.save()
-            response = "END Murakoze kwiyandikisha kuri Ida farm \n"
-
-
-        elif text == '1*2':
-            product ="Indimu"
-            response ="CON shyiramo ubuso bw'ubutaka bwawe bw' "+str(product)+"\n"
-        elif category =='1*2' and int(len(level)) == 3 and str(level[2]) in  str(level):
-            response = "CON Uwo mubufatanyije \n"
-        elif category =='1*2' and int(len(level)) == 4 and str(level[3]) in  str(level):
-            response = "CON Shyiramo nimero y'irangamuntu yuwo mufatanyije \n"
-        elif category =='1*2' and int(len(level)) == 5 and str(level[4]) in  str(level):
-            category='Indimu'
-            sizeOfland=level[2]
-            names= level[3]
-            idnumber = level[4]
-            insert = Idafarmuser(sessiondId=session_id,
-            serviceCode = service_code,
-            phoneNumber=phone_number,
-            level=level,
-            category=category,
-            sizeOfland=sizeOfland,
-            names=names,
-            idnumber=idnumber,
-            )
-            insert.save()
-            response = "END Murakoze kwiyandikisha kuri Ida farm \n"
-         
-        #  ======================== INGENGABIHE==================
-        elif text == '2':
-            response = "CON Hitamo igihe \n "
-            response += "1. Rimwe mukwezi \n"
-            response += "2. Kabiri Mukwezi \n"
-            response += "3. Buri gihe"
-        elif text == '2*1':
-            # save the data
-            insertData(
-                category='Rimwe',
-                sessionID=session_id,
-                phoneNumber=phone_number
-            )
-            response ="END Murakoze , tuzajya tubagezaho amakuru ku iteganyagihe rimwe mukwezi"
-        elif text == '2*2':
-            insertData(
-                category='Kabiri',
-                sessionID=session_id,
-                phoneNumber=phone_number
-            )
-            response ="END Murakoze , tuzajya tubagezaho amakuru ku iteganyagihe kabiri mukwezi"
-        elif text == '2*3':
-            insertData(
-                category='Burigihe',
-                sessionID=session_id,
-                phoneNumber=phone_number
-            )
-            response ="END Murakoze , tuzajya tubagezaho amakuru ku iteganyagihe Buri munsi"
+        # SELECT * FROM PRODUCTMODEL where title="" ORDER BY ID DESC LIMIT 5
+        #     fetchProducts = ProductsModel.objects.all()
+        #     response = "CON ANDIKA IMYIRONDORO YAWE \n"
+        #     for  products in fetchProducts:
+        #         response += ""+str(products.id)+"."+str(products.title)+ "\n"
+        #
+        # elif text == '1*1':
+        #     product="AMAZINA"
+        #     response = "CON shyiramo ubuso bw'ubutaka bwawe bw' "+str(product)+"\n"
+        # elif category =='1*1' and int(len(level)) == 3 and str(level[2]) in  str(level):
+        #     response = "CON Uwo mubufatanyije \n"
+        # elif category =='1*1' and int(len(level)) == 4 and str(level[3]) in  str(level):
+        #     response = "CON Shyiramo nimero y'irangamuntu yuwo mufatanyije \n"
+        # elif category =='1*1' and int(len(level)) == 5 and str(level[4]) in  str(level):
+        #     # save the data into the database
+        #     category='Ibinyomoro'
+        #     sizeOfland=level[2]
+        #     names= level[3]
+        #     idnumber = level[4]
+        #     insert = Idafarmuser(sessiondId=session_id,
+        #     serviceCode = service_code,
+        #     phoneNumber=phone_number,
+        #     level=level,
+        #     category=category,
+        #     sizeOfland=sizeOfland,
+        #     names=names,
+        #     idnumber=idnumber,
+        #     )
+        #     insert.save()
+        #     response = "END Murakoze kwiyandikisha kuri Ida farm \n"
+        #
+        #
+        # elif text == '1*2':
+        #     product ="Indimu"
+        #     response ="CON shyiramo ubuso bw'ubutaka bwawe bw' "+str(product)+"\n"
+        # elif category =='1*2' and int(len(level)) == 3 and str(level[2]) in  str(level):
+        #     response = "CON Uwo mubufatanyije \n"
+        # elif category =='1*2' and int(len(level)) == 4 and str(level[3]) in  str(level):
+        #     response = "CON Shyiramo nimero y'irangamuntu yuwo mufatanyije \n"
+        # elif category =='1*2' and int(len(level)) == 5 and str(level[4]) in  str(level):
+        #     category='Indimu'
+        #     sizeOfland=level[2]
+        #     names= level[3]
+        #     idnumber = level[4]
+        #     insert = Idafarmuser(sessiondId=session_id,
+        #     serviceCode = service_code,
+        #     phoneNumber=phone_number,
+        #     level=level,
+        #     category=category,
+        #     sizeOfland=sizeOfland,
+        #     names=names,
+        #     idnumber=idnumber,
+        #     )
+        #     insert.save()
+        #     response = "END Murakoze kwiyandikisha kuri Ida farm \n"
+        #
+        # #  ======================== INGENGABIHE==================
+        # elif text == '2':
+        #     response = "CON Hitamo igihe \n "
+        #     response += "1. Rimwe mukwezi \n"
+        #     response += "2. Kabiri Mukwezi \n"
+        #     response += "3. Buri gihe"
+        # elif text == '2*1':
+        #     # save the data
+        #     insertData(
+        #         category='Rimwe',
+        #         sessionID=session_id,
+        #         phoneNumber=phone_number
+        #     )
+        #     response ="END Murakoze , tuzajya tubagezaho amakuru ku iteganyagihe rimwe mukwezi"
+        # elif text == '2*2':
+        #     insertData(
+        #         category='Kabiri',
+        #         sessionID=session_id,
+        #         phoneNumber=phone_number
+        #     )
+        #     response ="END Murakoze , tuzajya tubagezaho amakuru ku iteganyagihe kabiri mukwezi"
+        # elif text == '2*3':
+        #     insertData(
+        #         category='Burigihe',
+        #         sessionID=session_id,
+        #         phoneNumber=phone_number
+        #     )
+        #     response ="END Murakoze , tuzajya tubagezaho amakuru ku iteganyagihe Buri munsi"
 
         else:
             response = "END Ukanze ibitaribyo, ongera mukanya"
